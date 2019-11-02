43b7d5808420a23dd2a715e812aa711b8d3afe69
zakhap <zhs2104@columbia.edu>
2019-11-02 17:33:27 -0400

    404 page styled

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,12 +2,15 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import styles from '../components/indexStyles.module.scss'
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>We can't find what you're looking for :(</p>
+    <div className={styles.fourOhFour}>
+        <h1>NOT FOUND</h1>
+        <p>We can't find what you're looking for :(</p>
+    </div>
   </Layout>
 )
 
